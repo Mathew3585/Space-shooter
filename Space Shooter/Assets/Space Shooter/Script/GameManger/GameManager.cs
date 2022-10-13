d0169bc84ace39ebf8b2694a98005ecb22d45b7f
Mathew3585 <68810417+Mathew3585@users.noreply.github.com>
2022-10-13 22:02:20 +0200

    Upadate

--- Space Shooter/Assets/Space Shooter/Script/GameManger/GameManager.cs
+++ Space Shooter/Assets/Space Shooter/Script/GameManger/GameManager.cs
@@ -8,6 +8,9 @@ using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Menu or Game")]
+    public bool Menu;
+    public bool Game;
     [Header("Value")]
     public int money;
 
@@ -25,32 +28,102 @@ public class GameManager : MonoBehaviour
 
     public Asteroid_Field asteroid_;
 
+    [Header("Gun")]
+    public bool Gunup1;
+    public bool Gunup2;
+    public bool Gunup3;
+    [Header("Gun GameObject")]
+    public GameObject Gun1;
+    public GameObject Gun2;
+    public GameObject Gun3;
+    [Header("Price Value")]
+    public float PriceGunUp2;
+    public float PriceGunUp3;
+    [Header("Price Text")]
+    public TextMeshProUGUI PriceGunUp2Text;
+    public TextMeshProUGUI PriceGunUp3Text;
+    [Header("Button Upgradde")]
+    public Button PriceGunUp2Button;
+    public Button PriceGunUp3Button;
+
     public void Awake()
     {
-        ship_Controller = GameObject.FindObjectOfType<Ship_Controller>();
-        UILose.SetActive(false);
-        UIInGame.SetActive(true);
-        ProgessSlider = gameObject.GetComponent<ProgerssionSlider>();
-        asteroid_ = GameObject.FindObjectOfType<Asteroid_Field>();
+        Gunup1 = true;
+        if (Menu == true)
+        {
+            if (Gunup1 == true)
+            {
+                Gun1.SetActive(true);
+                Gun2.SetActive(false);
+                Gun3.SetActive(false);
+            }
+            PriceGunUp2Text.text = PriceGunUp2.ToString();
+            PriceGunUp3Text.text = PriceGunUp3.ToString();
+
+            Debug.Log("Menu Activer");
+        }
+        if(Game == true)
+        {
+            ship_Controller = GameObject.FindObjectOfType<Ship_Controller>();
+            UILose.SetActive(false);
+            UIInGame.SetActive(true);
+            ProgessSlider = gameObject.GetComponent<ProgerssionSlider>();
+            asteroid_ = GameObject.FindObjectOfType<Asteroid_Field>();
+            Debug.Log("Game Activer");
+        }
+
     }
     void Update()
     {
-        moneyText.text = "" + money;
-        //PlayerPrefs.SetInt("SpaceShootherMoney", money);
+        if (Menu == true)
+        {
+            if (Gunup2 == true)
+            {
+                Gun1.SetActive(true);
+                Gun2.SetActive(true);
+            }
 
-        if(ship_Controller.stats.CurrentHealth <= 0)
-        {
-            UILose.SetActive(true);
-            UIInGame.SetActive(false);
+            if (Gunup3 == true)
+            {
+                Gun1.SetActive(true);
+                Gun2.SetActive(true);
+                Gun3.SetActive(true);
+            }
         }
-        if(ProgessSlider.Progress >= 70)
+
+        if(Game == true)
         {
-            asteroid_.enabled = false;
-            Debug.Log("Boss fight");
+            moneyText.text = "" + money;
+            //PlayerPrefs.SetInt("SpaceShootherMoney", money);
+
+            if (ship_Controller.stats.CurrentHealth <= 0)
+            {
+                UILose.SetActive(true);
+                UIInGame.SetActive(false);
+            }
+            if (ProgessSlider.Progress >= 70)
+            {
+                asteroid_.enabled = false;
+                Debug.Log("Boss fight");
+            }
         }
+
+    }
+
+    public void OnClickUpagrade2()
+    {
+        Gunup2 = true;
+        PriceGunUp2Button.enabled = false;
+    }
+    public void OnClickUpagrade3()
+    {
+        if(Gunup3 == true)
+        {
+            PriceGunUp2Button.enabled = false;
+        }
+        Gunup3 = true;
+        PriceGunUp3Button.enabled = false;
     }
 
 
-
-    
 }
