d12922a19f0fe25b425fcc8ff859a5b92892f927
Mathew3585 <68810417+Mathew3585@users.noreply.github.com>
2022-11-24 17:22:07 +0100

    Base Pégase

--- Space Shooter/Assets/Space Shooter/Script/Ennemis/Pégase/PégaseStats.cs
+++ Space Shooter/Assets/Space Shooter/Script/Ennemis/Pégase/PégaseStats.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PégaseStats : MonoBehaviour
 {
+    public Ennemis_Stats ennemis_Stats;
+    [Space(10)]
+    public float TimeTranslate;
+    public float speed;
+    public GameObject explosionPrefabs; 
+    private Transform target;
+    private Ship_Controller ship_Controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        ship_Controller = GameObject.FindGameObjectWithTag("Player").GetComponent<Ship_Controller>();
+        ennemis_Stats.currentHealth = ennemis_Stats.MaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 a = transform.position;
+        Vector3 b = target.position;
+        transform.position = Vector3.MoveTowards(a, Vector3.Lerp(a, b, TimeTranslate), speed);
+        transform.LookAt(target);
+        if (ennemis_Stats.currentHealth <= 0 )
+        {
+            Instantiate(explosionPrefabs, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (ship_Controller.ShieldActivate)
+            {
+                ship_Controller.shipStats.CurrentShield -= ennemis_Stats.dammage;
+                Destroy(gameObject);
+            }
+            else
+                ship_Controller.shipStats.CurrentHealth -= ennemis_Stats.dammage;
+                Destroy(gameObject);
+        }
     }
 }
